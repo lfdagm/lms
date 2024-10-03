646addfe6e8329dac214139718d90aa60619d556
lfdagm <lfdagm@gmail.com>
2024-10-02 21:35:26 -0700

    Adding UML diagram and plantUML code

---
+++ lms_a4/src/lms_a4/Lms_a4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lms_a4;
+
+/**
+ *
+ * @author lfdagm
+ */
+public class Lms_a4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
